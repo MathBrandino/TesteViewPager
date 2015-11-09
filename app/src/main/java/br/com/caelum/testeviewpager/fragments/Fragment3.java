e2e1462247a551c212634f0daabe79a1060d13e4
MathBrandino <m.v.brandino@gmail.com>
2015-11-09 15:16:10 -0200

    adicao de intent em imageview

--- app/src/main/java/br/com/caelum/testeviewpager/fragments/Fragment3.java
+++ app/src/main/java/br/com/caelum/testeviewpager/fragments/Fragment3.java
@@ -1,7 +1,9 @@
 package br.com.caelum.testeviewpager.fragments;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -29,6 +31,14 @@ public class Fragment3 extends Fragment {
         Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.caelum);
         bm = Bitmap.createScaledBitmap(bm, 300, 300, true);
         imageView.setImageBitmap(bm);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://caelum.com.br"));
+                startActivity(intent);
+            }
+        });
         view.addView(imageView);
 
         return view;
